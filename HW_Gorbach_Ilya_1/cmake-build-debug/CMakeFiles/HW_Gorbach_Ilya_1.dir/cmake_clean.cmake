88cf08ef35e652f37adc322d1fb269b83f185a9f
ilya <ivgorbach@edu.hse.ru>
2020-12-02 15:57:05 +0300

    Gdal time

---
+++ HW_Gorbach_Ilya_1/cmake-build-debug/CMakeFiles/HW_Gorbach_Ilya_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_Gorbach_Ilya_1.dir/main.cpp.obj"
+  "HW_Gorbach_Ilya_1.exe"
+  "HW_Gorbach_Ilya_1.exe.manifest"
+  "HW_Gorbach_Ilya_1.pdb"
+  "libHW_Gorbach_Ilya_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_Gorbach_Ilya_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
